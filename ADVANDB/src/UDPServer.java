4c469df51c4b4e78ec360aa0cdc7863404828a2b
Brandon <b.u.penaranda@gmail.com>
2016-04-02 14:01:15 +0800

    Explanation of udp code
    hellor is it me

--- ADVANDB/src/UDPServer.java
+++ ADVANDB/src/UDPServer.java
@@ -9,15 +9,20 @@ class UDPServer
 		byte[] sendData = new byte[1024];
 		while(true)                
 		{
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);                   		
+			//Creates a socket for receiving packets
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);    
+			//Created socket receives packet
 			serverSocket.receive(receivePacket);                   
 			String sentence = new String( receivePacket.getData());                   
-			System.out.println("RECEIVED: " + sentence);                   
+			System.out.println("RECEIVED: " + sentence);
+			//Using received packet I get address and port number
 			InetAddress IPAddress = receivePacket.getAddress();                   
 			int port = receivePacket.getPort();                   
+			//Send back sentence as acknowledgement/reply
 			String capitalizedSentence = sentence.toUpperCase();                  
 			capitalizedSentence.concat(" Pogi si glenn");
 			sendData = capitalizedSentence.getBytes();     
+			//Send acknowledgement to whom pack is from
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
 			serverSocket.send(sendPacket);                
 		}       
