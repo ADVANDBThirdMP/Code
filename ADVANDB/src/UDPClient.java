0cf9905a365f2ab9381c1d776e466ad775e2b9a5
Calvin-Glein <glenn_matias@dlsu.edu.ph>
2016-04-02 14:08:32 +0800

    Added spaces

--- ADVANDB/src/UDPClient.java
+++ ADVANDB/src/UDPClient.java
@@ -7,18 +7,23 @@ class UDPClient
 	{       
 		//Gets input from user
 				BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));     
+				
 				//Makes socket used to send 
 				DatagramSocket clientSocket = new DatagramSocket();       
+				
 				//Too whom it is sent
 				InetAddress IPAddress = InetAddress.getByName("localhost");       
 				byte[] sendData = new byte[1024];       
 				byte[] receiveData = new byte[1024];       
 				String sentence = inFromUser.readLine();
+				
 				//converts string to bytes
 				sendData = sentence.getBytes();       
+				
 				//Send it to server
 				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);       
 				clientSocket.send(sendPacket);       
+				
 				//Waits for reply/acknowledgement from server
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);       
 				clientSocket.receive(receivePacket);       
